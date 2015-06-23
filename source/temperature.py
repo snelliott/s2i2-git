8b21ca0588b3194e4df75673e496ae73e264e390
Avery Wiens <avery@Roger.local>
2015-06-23 11:17:52 -0400

    add temp k_to_c and f_to_c functions and tests

--- source/temperature.py
+++ source/temperature.py
@@ -3,4 +3,15 @@
 # python script that converts between units of temperature
 
 def f_to_k(temp):
-   return (temp-32)*(5/9.0)+273
+   return (temp-32)*(5/9.0)+273.15
+
+def k_to_c(temp):
+   return temp - 273.15
+
+def f_to_c(temp):
+   return k_to_c(f_to_k(temp))
+
+# tests
+print f_to_k(32)
+print k_to_c(273.15)
+print f_to_c(32)
